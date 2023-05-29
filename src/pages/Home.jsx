260bfffca7e926bb84e5bb6502cc5c73cb9b69f7
Varzika Tiwari <varzikat@gmail.com>
2023-05-29 23:19:46 +0530

    changed the localhosturl to render one

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
       setLoading(true);
 
       try{
-        const response = await fetch('http://loaclhost:8080/api/v1/post',{
+        const response = await fetch('https://dall-e-q0lq.onrender.com/api/v1/post',{
           method: 'GET',
           headers: {
             'Content-Type' : 'application/json',
